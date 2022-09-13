445ca3f93d88a598b281b966afed65ec5145d391
Thejairex <Yairjesus777@gmail.com>
2022-09-13 04:42:58 -0300

    Conexion con la base de datos, y algunos templates de prueba

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
 from flask_mysqldb import MySQL
+import hashlib
 
 
-def prueba():
-    cur = mysql.connection.cursor()
-    query = "SELECT * FROM user"
-    cur.execute(query)
-    data = cur.fetchone()
-    return str(data)
+def insert_user(username,password):
+    try:
+        cur=mysql.connection.cursor()
+        query="INSERT INTO user (username,password) VALUES('{}','{}')".format(username,password)
+        cur.execute(query)
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+    except Exception as e:
+        return str(e)
+
+def fetch_users():
+    try:
+        cur=mysql.connection.cursor()
+        query="SELECT * FROM user"
+        cur.execute(query)
+        data=cur.fetchall()
+        return data
+    except Exception as e:
+        return str(e)
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -17,9 +31,13 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'museo'
 
+app.secret_key = "mysecretkey"
+
+
 @app.route('/')
 def index():
-    return prueba()
+    datas = fetch_users()
+    return render_template('index.html', datas=datas)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
