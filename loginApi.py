94623aeaa7116b7b4ec7abae9819fdfc142aeabf
Thejairex <Yairjesus777@gmail.com>
2022-09-14 01:01:58 -0300

    New Branch 2

---
+++ loginApi.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint, jsonify
+from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required, set_access_cookies, unset_jwt_cookies
+from querys import Query
+from main import jwt
+
+import hashlib
+
+loginApi = Blueprint('loginApi', __name__, template_folder='app/templates')
+
+
+@loginApi.route("/api/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        userData = Query.fetch_user_login(username, password)
+        if userData != None:
+            access_token = create_access_token(identity={
+                'username': userData[0],
+                'email': userData[2],
+                'role': userData[3],
+            })
+
+            response = jsonify({
+                    'accessToken': access_token,
+                    'username': userData[0],
+                    'email': userData[2],
+                    'role': userData[3],
+                })
+            set_access_cookies(response, access_token)
+            return response
+        else:
+            return jsonify({
+                "message": "User not found!" 
+            }), 401
+
+@loginApi.route("/user", methods=['GET'])
+@jwt_required()
+def user():
+    current_user = get_jwt_identity
+    return jsonify(current_user), 200
+
+
+@loginApi.route("/api/signup", methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        if Query.fetch_user_signup(username=username, email=None):
+            if Query.fetch_user_signup(username=None, email=email):
+                userData= Query.insert_user(username,password,email)
+                access_token = create_access_token(identity={
+                    'user': userData[0],
+                    'email': userData[2],
+                    'role': userData[3],
+                })
+
+                response = jsonify({
+                    'accessToken': access_token,
+                    'username': userData[0],
+                    'email': userData[2],
+                    'role': userData[3],
+                })
+
+                set_access_cookies(response, access_token)
+                return response
+
+            else:
+                return jsonify({
+                "message": "Your email is registered"
+            }), 401
+        else:
+            return jsonify({
+                "message": "Your username is registered"
+            }), 401
