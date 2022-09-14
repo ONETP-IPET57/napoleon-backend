6ac2bcb7a07c59c58d558f10628194b3d9534a50
Thejairex <Yairjesus777@gmail.com>
2022-09-14 03:15:59 -0300

    System CRUD exhibition API

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import time
 from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint, jsonify
 from flask_mysqldb import MySQL
 from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required, set_access_cookies, unset_jwt_cookies
@@ -31,6 +30,54 @@ def login_page():
 @app.route('/signup')
 def signup_page():
     return render_template('signup.html')
+
+@app.route('/api/exhibitions', methods=['GET'])
+def exhibitions():
+    if request.method == 'GET':
+        dataExhibitions = Query.fetch_all_exhibition()
+        jsonExhibitions = []
+
+        for data in dataExhibitions:
+            jsonExhibitions.append({
+                'id_exhibition': data[0],
+                'name_exhibition': data[1],
+                'author': data[2],
+                'created_at': data[3],
+                'information': data[4],
+                'image': data[5]
+            })
+        return jsonify(jsonExhibitions)
+
+@app.route('/api/exhibition/<id>', methods=['POST','GET','DELETE'])
+@jwt_required(locations='cookies')
+def exhibition(id):
+    if get_jwt_identity().role != 'administrator':
+        return jsonify({'message': 'Not authorized'}), 403
+
+    if request.method == 'GET':
+        dataExhibition = Query.fetch_exhibition(id)
+        return jsonify({
+            'id_exhibition': dataExhibition[0],
+            'name_exhibition': dataExhibition[1],
+            'author': dataExhibition[2],
+            'created_at': dataExhibition[3],
+            'information': dataExhibition[4],
+            'image': dataExhibition[5]
+        })
+    
+    if request.method == 'DELETE':
+        return jsonify(Query.delete_exhibition(id))
+    
+    if request.method == 'POST':
+        id_exhibition = id
+        name_exhibition = request.json.get("name_exhibition", None)
+        author = request.json.get("author", None)
+        created_at = request.json.get("created_at", None)
+        information = request.json.get("information", None)
+        image = request.json.get("image", None)
+
+        return jsonify(Query.update_exhibition(id_exhibition,name_exhibition, author, created_at, information, image))
+
 
 app.register_blueprint(loginApi)
 
--- querys.py
+++ querys.py
@@ -50,7 +50,6 @@ class Query():
     @classmethod
     def fetch_user_login(self, username,password):
         cur=mysql.connection.cursor()
-        print(username, password, "hola")
         query="SELECT u.username, u.password, u.email, r.role FROM user u INNER JOIN role r on u.id_role = r.id_role WHERE u.username='{}'".format(username)
         cur.execute(query)
         data=cur.fetchone()
@@ -59,3 +58,48 @@ class Query():
             return data
         else: 
             return None
+
+# QUERYS MOD EXHIBITION
+
+    @classmethod
+    def insert_exhibition(self, name_exhibition, author, created_at, information, image):
+        try:
+            cur=mysql.connection.cursor()
+            query="INSERT INTO exhibition (name_exhibition,author,created_at,information,image) VALUES('{}','{}',{},'{}','{}',)".format(name_exhibition, author, created_at, information, image)
+            cur.execute(query)
+            return {'message': 'Added Successfully'}
+        except Exception as e:
+            return str(e)
+
+    @classmethod
+    def update_exhibition(self, id, name_exhibition, author, created_at, information, image):
+        try:
+            cur=mysql.connection.cursor()
+            query="UPDATE exhibition SET (name_exhibition,author,created_at,information,image) VALUES('{}','{}',{},'{}','{}',) WHERE id_exhibition={}".format(name_exhibition, author, created_at, information, image,id)
+            cur.execute(query)
+            return {'message': 'Updated Successfully'}
+        except Exception as e:
+            return str(e)
+
+    @classmethod
+    def fetch_all_exhibition(self):
+        cur=mysql.connection.cursor()
+        query="SELECT * FROM exhibition"
+        cur.execute(query)
+        data = cur.fetchall()
+        return data
+
+    @classmethod
+    def fetch_exhibition(self, id):
+        cur=mysql.connection.cursor()
+        query="SELECT * FROM exhibition where id_exhibition={}".format(id)
+        cur.execute(query)
+        data = cur.fetchone()
+        return data
+
+    @classmethod
+    def delete_exhibition(self, id):
+        cur=mysql.connection.cursor()
+        query="DELETE FROM exhibition WHERE id={}".format(id)
+        cur.execute(query)
+        return {'message': 'Delete Successfully'}
